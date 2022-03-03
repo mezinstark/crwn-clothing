d61c1ab344b6db4a41f3b0e8892132b2bad5084e
Mezin Stark <54954471+mezinstark@users.noreply.github.com>
2022-03-03 15:09:32 +0530

    Shop-Preview Page:added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
+import ShopPage from './pages/shop/shop.components';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+      
     </div>
   );
 }
